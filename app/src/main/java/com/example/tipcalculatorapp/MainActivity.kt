5e3fe788344b8b23a7365d207d8c52f8f00aeefc
Ibrahim Duman <dumanibrahm@gmail.com>
2023-03-24 15:04:59 +0300

    Kod bağlantıları yapıldı.

--- app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
@@ -7,13 +7,35 @@ import com.example.tipcalculatorapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-
-
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener(){
+            calculateButton()
+        }
+    }
+    @SuppressLint("StringFormatInvalid")
+    fun calculateButton(){
+        val stringInTextField = binding.cost.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when(selectedId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        //val roundUp = binding.roundUpSwitch.isChecked
+        if(binding.roundUpSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
 
     }
-
 
 
 }
