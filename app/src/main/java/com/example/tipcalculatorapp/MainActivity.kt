fe2bdead09d047edde825164d0c8e9d13271eb1e
Ibrahim Duman <dumanibrahm@gmail.com>
2023-03-24 15:02:16 +0300

    Kullanıcı arayüzü tamamlandı.

---
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tipcalculatorapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculatorapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+
+}
